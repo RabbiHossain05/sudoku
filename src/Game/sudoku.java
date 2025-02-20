b993f7b65271cd511dfc903c7918b69b84836473
EmanueleCervone <emanuelecervone@itsincom.it>
2025-02-20 16:18:02 +0100

    Creazione

---
+++ src/Game/sudoku.java
@@ -0,0 +1,29 @@
+package Game;
+
+import java.io.*;
+import java.util.*;
+
+
+public class sudoku {
+
+        public static boolean verificaVittoria(int[][] board) {
+
+            return true;
+        }
+
+        private static boolean checkQuadrati(int[][] board) {
+
+            return true;
+        }
+
+        private static boolean checkUnicita(int[] numeri) {
+
+            return true;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+
+
